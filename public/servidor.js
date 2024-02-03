d901a9e225db998c9a67c3372ad3324ac8db0fcc
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2024-02-03 16:26:50 -0300

    creating server

---
+++ public/servidor.js
@@ -0,0 +1,8 @@
+const http = require('http'); // facilita a configuração de um servidor htttp por meio de requisição uma variavel 
+const express = require('express');
+const app = express();
+const servidorHttp = http.createServer(aplicacao);
+
+servidorHttp.listen(1000);
+//aplicação backend de js, rodada pelo node
+n
