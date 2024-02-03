b4dc62ce7b9cc910fd28ec2273032558c5a66adc
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2024-02-03 20:01:19 -0300

    update

--- public/servidor.js
+++ public/servidor.js
@@ -1,8 +1,17 @@
 const http = require('http'); // facilita a configuração de um servidor htttp por meio de requisição uma variavel 
 const express = require('express');
-const app = express();
+const aplicacao = express();
+
 const servidorHttp = http.createServer(aplicacao);
+const io = require('soquet.io')(servidorHttp)
+
+botao.addEventListener('click')
+io.addListener('connection', () =>  {
+    console.log('um usuario conectou');
+})
+
+
 
 servidorHttp.listen(1000);
 //aplicação backend de js, rodada pelo node
-n
+aplicacao.use(express.static('public'))
